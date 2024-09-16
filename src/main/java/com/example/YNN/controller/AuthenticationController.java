6d39429a4039b321c570988466939fd96156c903
tkv00 <tkv0098@gmail.com>
2024-09-17 06:58:56 +0900

    ✨feat:사용자 인가 api 구현

--- src/main/java/com/example/YNN/controller/AuthenticationController.java
+++ src/main/java/com/example/YNN/controller/AuthenticationController.java
@@ -2,6 +2,11 @@ package com.example.YNN.controller;
 
 import com.example.YNN.DTO.LoginRequestDTO;
 import com.example.YNN.service.AuthServiceImpl;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -15,12 +20,24 @@ import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
-
+@Tag(name = "로그인",description = "로그인 API")
 public class AuthenticationController {
     private final AuthServiceImpl authService;
 
     //로그인 하기
     @PostMapping("/api/login")
+    @Operation(
+            summary = "로그인",
+            description = "회원 로그인 하기",
+            responses = {
+                    @ApiResponse(responseCode = "200",description = "로그인 성공 및 JWT반환",
+                        content = @Content(
+                                mediaType = "application/json",
+                                schema =@Schema(example = "{\"token\": \"유저의 jwt String값\"}")
+                        )),
+                    @ApiResponse(responseCode = "400",description = "로그인 실패")
+            }
+    )
     public ResponseEntity<Map<String,String>>getUserProfile(@Valid @RequestBody LoginRequestDTO loginRequestDTO){
         String token=this.authService.login(loginRequestDTO);
         Map<String,String> response=new HashMap<>();
