d95eacb1238fc8991645d54b8240f53957864f2c
Park Jae Seong <timer973@naver.com>
2024-09-16 16:10:36 +0900

    test

---
+++ src/main/java/com/example/YNN/YnnApplication.java
@@ -0,0 +1,13 @@
+package com.example.YNN;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YnnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YnnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/YNN/YnnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.YNN;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YnnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
