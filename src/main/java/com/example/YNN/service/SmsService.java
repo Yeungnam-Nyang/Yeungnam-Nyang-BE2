637b911c5ff568beef3d7b1b8f387163c3008bc9
tkv00 <tkv0098@gmail.com>
2024-09-16 19:31:35 +0900

    ✨feat:임시 비밀번호 sms 전송구현

--- src/main/java/com/example/YNN/service/SmsService.java
+++ src/main/java/com/example/YNN/service/SmsService.java
@@ -1,5 +1,6 @@
 package com.example.YNN.service;
 
+import com.example.YNN.DTO.FindPasswordDTO;
 import com.example.YNN.DTO.SmsCertificationDTO;
 import com.example.YNN.repository.SmsCertificationDao;
 import net.nurigo.java_sdk.exceptions.CoolsmsException;
@@ -8,4 +9,6 @@ public interface SmsService {
     void sendSms(String to)  throws CoolsmsException;
 
     void verifySms(SmsCertificationDTO smsCertificationDTO);
+
+    void sendNewPassword (FindPasswordDTO findPasswordDTO) throws CoolsmsException;
 }
