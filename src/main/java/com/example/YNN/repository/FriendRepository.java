4e625ea900c8e75cbc031051eaff161f7b94707e
Park Jae Seong <timer973@naver.com>
2024-10-09 19:27:52 +0900

    ♻️refact:Repository Transaction과 Implement Transaction 중복 조회기능 제거

--- src/main/java/com/example/YNN/repository/FriendRepository.java
+++ src/main/java/com/example/YNN/repository/FriendRepository.java
@@ -9,15 +9,12 @@ import java.util.List;
 
 public interface FriendRepository extends JpaRepository<Friend, Long> {
 
-    @Transactional(readOnly = true) // 특정 유저가 보낸 한 명의 친구 요청 조회
     Friend findByUser_UserIdAndFriendId(String userId, String friendId);
 
-    @Transactional(readOnly = true) // 특정 유저의 친구 목록 조회 (수락 된 친구 목록)
     List<Friend> findByUser_UserIdAndStatus(String userId, FriendRequestStatus status);
 
 //    @Transactional(readOnly = true)
 //    List<Friend> findByUser_UserId(String userId);
 
-    @Transactional(readOnly = true) // 특정 유저가 보낸 모든 친구 요청 조회
     List<Friend> findByUser_UserIdAndStatusIn(String userId, List<FriendRequestStatus> statuses);
 }
