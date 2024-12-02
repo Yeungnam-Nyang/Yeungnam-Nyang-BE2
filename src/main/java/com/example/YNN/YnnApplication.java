a3f4fbac04d3e59e1bae4015fe4f1250810392b6
Park Jae Seong <timer973@naver.com>
2024-12-03 01:44:43 +0900

    SpringBootServletInitializer

--- src/main/java/com/example/YNN/YnnApplication.java
+++ src/main/java/com/example/YNN/YnnApplication.java
@@ -8,12 +8,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-public class YnnApplication {
+public class YnnApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(YnnApplication.class, args);
